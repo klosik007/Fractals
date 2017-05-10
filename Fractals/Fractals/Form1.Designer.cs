6399f891edec022f3d95d3f395dc536dc157f73e
Przemysław K <pklos1992@gmail.com>
2017-05-10 11:07:13 +0200

    Add files via upload

---
+++ Fractals/Fractals/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Fractals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.resolutionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(349, 27);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(107, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Sierpinski triangle";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(349, 50);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(93, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Sierpinski lane";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(349, 73);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(73, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Cantor set";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(349, 96);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(80, 17);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Koch curve";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(347, 257);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(346, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Iterations:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(465, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.resolutionToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(168, 22);
+            this.toolStripMenuItem2.Text = "Save to file...";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // resolutionToolStripMenuItem
+            // 
+            this.resolutionToolStripMenuItem.Name = "resolutionToolStripMenuItem";
+            this.resolutionToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.resolutionToolStripMenuItem.Text = "Output resolution";
+            this.resolutionToolStripMenuItem.Click += new System.EventHandler(this.resolutionToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(330, 330);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(345, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Save to file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(349, 119);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(95, 17);
+            this.radioButton5.TabIndex = 12;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Mandelbrot set";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(349, 143);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(85, 17);
+            this.radioButton6.TabIndex = 13;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "radioButton6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(349, 167);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(85, 17);
+            this.radioButton7.TabIndex = 14;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "radioButton7";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 364);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Fractals";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem resolutionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+    }
+}
+
---
+++ Fractals/Fractals/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+
+namespace Fractals
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private Pen pen;
+
+        private const byte N = 255; //num of iterations
+        //public double xMin, xMax, yMin, yMax;//coordinates min and max
+
+        /*  void setBitmapProperties(int resW, int resH)
+          {
+               // this.resW = resW;                                   
+              // this.resH = resH;                                    
+             //  bm = new Bitmap(resW, resH);                                    
+
+               }*/
+        /*public struct Complex
+        {
+            public double Re, Im;//real part, imaginary part
+        }
+        public Complex CSum(Complex A, Complex B)
+        {
+            Complex sum = new Complex();
+            sum.Re = A.Re + B.Re;
+            sum.Im = A.Im + B.Im;
+            return sum;
+        }
+
+        public Complex CRatio(Complex A, Complex B)
+        {
+            Complex ratio = new Complex();
+            ratio.Re = A.Re * B.Re - A.Im * B.Im;
+            ratio.Im = A.Re * B.Im + A.Im * B.Re;
+            return ratio;    
+        }
+
+        public Complex CSquare(Complex A)
+        {
+            return CRatio(A, A);
+        }
+
+        public double CModulo(Complex A)
+        {
+            return Math.Sqrt(A.Re * A.Re + A.Im * A.Im);
+        }*/
+
+    
+        
+        void DrawSierpinskisLane(double X, double Y, double W, int N, PictureBox pb)
+        {
+            pen = new Pen(Color.Black);
+            Rectangle r = new Rectangle((int)X, (int)Y, (int)W, (int)W);
+            double D = W / 3;
+
+            g = pb.CreateGraphics();
+            g.DrawRectangle(pen, r);
+            if (N < int.Parse(textBox1.Text))
+            {
+                DrawSierpinskisLane(X, Y, D, N + 1, pb);
+                DrawSierpinskisLane(X + D, Y, D, N + 1, pb);
+                DrawSierpinskisLane(X + 2 * D, Y, D, N + 1, pb);
+
+                DrawSierpinskisLane(X, Y + D, D, N + 1, pb);
+                DrawSierpinskisLane(X + 2 * D, Y + D, D, N + 1, pb);
+
+                DrawSierpinskisLane(X, Y + 2 * D, D, N + 1, pb);
+                DrawSierpinskisLane(X + D, Y + 2 * D, D, N + 1, pb);
+                DrawSierpinskisLane(X + 2 * D, Y + 2 * D, D, N + 1, pb);
+            }
+            pen.Dispose();
+            g.Dispose();
+        }
+
+        void drawSierpinskisTriangle(float x, float y, float d, int n, PictureBox pb)
+        {
+          
+            pen = new Pen(Color.Black);
+            g = pb.CreateGraphics();
+           
+            g.DrawLine(pen, x, y, x + d, y);
+            g.DrawLine(pen, x + d, y, x + d / 2, (float)(y - d * Math.Sqrt(3) / 2));
+            g.DrawLine(pen, x + d / 2, (float)(y - d * Math.Sqrt(3) / 2), x, y);
+
+            if (n < int.Parse(textBox1.Text))
+            {
+                drawSierpinskisTriangle(x, y, d / 2, n + 1, pb);
+                drawSierpinskisTriangle(x + d / 2, y, d / 2, n + 1, pb);
+                drawSierpinskisTriangle(x + d / 4, (float)(y - Math.Sqrt(3) * d / 4), d / 2, n + 1, pb);
+            }
+            pen.Dispose();
+            g.Dispose();
+        }
+
+        void drawKochsCurve(float x1, float y1, float x2, float y2, int n, PictureBox pb)
+        {
+            float dx = x2 - x1,
+                  dy = y2 - y1;
+            pen = new Pen(Color.Black);
+            g = pb.CreateGraphics();
+            
+            if (n == int.Parse(textBox1.Text))
+            {
+                 g.DrawLine(pen, x1, y1, x2, y2);
+            }
+            else
+            {
+                drawKochsCurve(x1, y1, x1 + dx / 3, y1 + dy / 3, n + 1, pb);
+                drawKochsCurve(x1 + dx * 2 / 3, y1 + dy * 2 / 3, x2, y2, n + 1, pb);
+                drawKochsCurve(x1 + dx / 3, y1 + dy / 3, (float)((x1 + x2) / 2 + 
+                    Math.Sqrt(3) / 6 * dy), 
+                    (float)((y1 + y2) / 2 - (Math.Sqrt(3) / 6) * dx), n + 1, pb);
+                drawKochsCurve((float)((x1 + x2) / 2 + (Math.Sqrt(3) / 6) * dy),
+                    (float)((y1 + y2) / 2 - Math.Sqrt(3) / 6 * dx),
+                    x1 + dx * 2 / 3, y1 + dy * 2 / 3, n + 1, pb);
+            }
+            pen.Dispose();
+            g.Dispose();
+        }
+
+        void drawCantorSet(int left, int right, int n, PictureBox pb)
+        {
+            pen = new Pen(Color.Black);
+            g = pb.CreateGraphics();
+
+            Rectangle r = new Rectangle(left, pb.Height - 10 - n * 5,
+                                        right, 4);
+            g.DrawRectangle(pen, r);
+            int d;
+            if (n < int.Parse(textBox1.Text) && (left < right))
+            {
+                d = (right - left) / 3;
+                drawCantorSet(left, left + d, n + 1, pb);
+                drawCantorSet(right - d , right-(right- d), n + 1, pb);// tu jest problem
+            }
+            
+            pen.Dispose();
+            g.Dispose();
+        }
+
+        void drawMandelbrotSet(PictureBox pb)
+        {
+            double xMin = -2; double xMax = 1;
+            double yMin = -1.5; double yMax = 1.5;
+            int I;
+            double dX, dY;
+
+            Complex c;
+            Complex p;
+           
+            pb.Image = new Bitmap(pb.Width, pb.Height);
+            g = pb.CreateGraphics();
+
+            dX = (xMax - xMin) / pb.Width;
+            dY = (yMax - yMin) / pb.Height;
+
+            for (int i = 0; i < pb.Width; i++)
+            {
+                for (int j =0; j < pb.Height; j++)
+                {
+                    c = new Complex(xMin + i * dX, yMin + j * dY);
+                    p = new Complex(0, 0);
+
+                    I = 0;
+                    do
+                    {
+                        p = Complex.Add(Complex.Pow(p,2), c);
+                        I++;
+                    } while((I>N) || (Complex.Abs(p) >= 2));
+                   
+                    if (I >= N)
+                        ((Bitmap)pb.Image).SetPixel(i, j, Color.White);
+                    else
+                        ((Bitmap)pb.Image).SetPixel(i, j, Color.Black);
+                }
+            }
+            g.Dispose();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.Paint += pictureBox1_Paint;
+            
+            pictureBox1.Refresh();
+            if (radioButton1.Checked)
+                drawSierpinskisTriangle(0, (pictureBox1.Height - 10), pictureBox1.Width, 1, pictureBox1);
+            if (radioButton2.Checked)
+                DrawSierpinskisLane(0, 0, pictureBox1.Width-10, 1, pictureBox1);
+            if (radioButton3.Checked)
+                drawCantorSet(0, pictureBox1.Width - 10, 1, pictureBox1);
+            if (radioButton4.Checked)
+                drawKochsCurve(10, pictureBox1.Height - 10, pictureBox1.Width - 10, pictureBox1.Height - 10, 1, pictureBox1);
+            if (radioButton5.Checked)
+                drawMandelbrotSet(pictureBox1);
+        }
+
+        private void resolutionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.Show();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {/*
+            if (radioButton1.Checked)
+                drawSierpinskisTriangle(0, (pictureBox1.Height - 10), pictureBox1.Width, 1, pictureBox1);
+            if (radioButton2.Checked)
+                DrawSierpinskisLane(0, 0, pictureBox1.Width - 10, 1, pictureBox1);
+            if (radioButton3.Checked)
+                drawCantorSet(0, pictureBox1.Width - 10, 1, pictureBox1);
+            if (radioButton4.Checked)
+                drawKochsCurve(10, pictureBox1.Height - 10, pictureBox1.Width - 10, pictureBox1.Height - 10, 1, pictureBox1);
+            if (radioButton5.Checked)
+                drawMandelbrotSet(pictureBox1);*/
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 frm1 = new Form1();
+            frm1.Close();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Bitmap bm = new Bitmap(pictureBox1.BackgroundImage);
+            SaveFileDialog dialog = new SaveFileDialog();
+            PictureBox pb1 = new PictureBox();
+            pb1.Image = new Bitmap(pb1.Width, pb1.Height);//aha...
+            pb1.Width = 1366;
+            pb1.Height = 768;
+            DrawSierpinskisLane(0, 0, pb1.Width, 1, pb1);
+            dialog.Filter = "bmp|*.bmp";
+            dialog.ShowDialog();
+            if (dialog.FileName != "")       // JAK TO ZROBIC???? ZEBY NIE BYLO NULL???
+                                             // bm.Save(dialog.FileName);
+                pb1.Image.Save(dialog.FileName);
+            /*SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "bmp|*.bmp";
+            dialog.ShowDialog();
+            if (dialog.FileName != "")
+                // bm.Save(dialog.FileName);*/
+            /*  PictureBox pb = new PictureBox();
+              pb.Image = new Bitmap("C:\\Users\\pklos\\Desktop\\cos.bmp");
+                  pb.Image.Save("C:\\Users\\pklos\\Desktop\\cos.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+        */
+        }
+    }
+}
